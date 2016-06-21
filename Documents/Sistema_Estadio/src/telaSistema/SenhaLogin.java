ab1f1babdc6c8b41824fc379e5d1e6e020851b24
sesi <sesi@IELLABINF01-23.labs.famecpr>
2016-06-21 12:54:18 -0300

    tela de login (feat luan)

--- Documents/Sistema_Estadio/src/telaSistema/SenhaLogin.java
+++ Documents/Sistema_Estadio/src/telaSistema/SenhaLogin.java
@@ -23,8 +23,9 @@ public class SenhaLogin extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         PASSWORD = new javax.swing.JPasswordField();
         jLabel2 = new javax.swing.JLabel();
-        SHOW = new javax.swing.JRadioButton();
         SEND = new javax.swing.JButton();
+        SEND1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -35,22 +36,14 @@ public class SenhaLogin extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("LOGIN");
+        jLabel1.setText("Login: ");
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel2.setText("PASSWORD");
-
-        SHOW.setForeground(new java.awt.Color(51, 51, 51));
-        SHOW.setText("SHOW PASSWORD");
-        SHOW.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SHOWActionPerformed(evt);
-            }
-        });
+        jLabel2.setText("Senha:");
 
         SEND.setForeground(new java.awt.Color(51, 51, 51));
-        SEND.setText("SEND");
+        SEND.setText("Entrar");
         SEND.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 SENDMouseClicked(evt);
@@ -62,6 +55,27 @@ public class SenhaLogin extends javax.swing.JFrame {
             }
         });
 
+        SEND1.setForeground(new java.awt.Color(51, 51, 51));
+        SEND1.setText("Sair");
+        SEND1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                SEND1MouseClicked(evt);
+            }
+        });
+        SEND1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SEND1ActionPerformed(evt);
+            }
+        });
+        SEND1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                SEND1KeyTyped(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel3.setText("Sistema estádio");
+
         javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
         jDesktopPane1.setLayout(jDesktopPane1Layout);
         jDesktopPane1Layout.setHorizontalGroup(
@@ -69,46 +83,51 @@ public class SenhaLogin extends javax.swing.JFrame {
             .addGroup(jDesktopPane1Layout.createSequentialGroup()
                 .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                        .addGap(64, 64, 64)
-                        .addComponent(SHOW)
-                        .addGap(39, 39, 39)
-                        .addComponent(SEND, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                        .addGap(186, 186, 186)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                        .addGap(113, 113, 113)
+                        .addGap(29, 29, 29)
                         .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(LOGIN, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(PASSWORD, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(88, Short.MAX_VALUE))
+                            .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(PASSWORD, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(LOGIN, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                                .addComponent(SEND, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(74, 74, 74)
+                                .addComponent(SEND1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                        .addGap(122, 122, 122)
+                        .addComponent(jLabel3)))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
         jDesktopPane1Layout.setVerticalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel3)
                 .addGap(43, 43, 43)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(LOGIN, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(PASSWORD, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
+                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LOGIN, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(46, 46, 46)
+                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
+                    .addComponent(PASSWORD))
+                .addGap(56, 56, 56)
                 .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(SEND, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(SHOW))
-                .addContainerGap(79, Short.MAX_VALUE))
+                    .addComponent(SEND1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
         jDesktopPane1.setLayer(LOGIN, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jDesktopPane1.setLayer(jLabel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jDesktopPane1.setLayer(PASSWORD, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jDesktopPane1.setLayer(jLabel2, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        jDesktopPane1.setLayer(SHOW, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jDesktopPane1.setLayer(SEND, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPane1.setLayer(SEND1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPane1.setLayer(jLabel3, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         getContentPane().add(jDesktopPane1);
         jDesktopPane1.setBounds(0, 0, 440, 350);
@@ -116,16 +135,6 @@ public class SenhaLogin extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
     
-    private void SHOWActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SHOWActionPerformed
-        
-        if(SHOW.isSelected()== true){
-            PASSWORD.setEchoChar((char)0);
-        }else{
-            PASSWORD.setEchoChar(('*'));
-        }
-        
-    }//GEN-LAST:event_SHOWActionPerformed
-
     private void SENDMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SENDMouseClicked
         metodoEntrar();
     }//GEN-LAST:event_SENDMouseClicked
@@ -133,6 +142,18 @@ public class SenhaLogin extends javax.swing.JFrame {
     private void SENDKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SENDKeyTyped
         metodoEntrar();
     }//GEN-LAST:event_SENDKeyTyped
+
+    private void SEND1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SEND1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_SEND1MouseClicked
+
+    private void SEND1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SEND1KeyTyped
+        // TODO add your handling code here:
+    }//GEN-LAST:event_SEND1KeyTyped
+
+    private void SEND1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SEND1ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_SEND1ActionPerformed
 
   
     public static void main(String args[]) {
@@ -171,10 +192,11 @@ public class SenhaLogin extends javax.swing.JFrame {
     private javax.swing.JTextField LOGIN;
     private javax.swing.JPasswordField PASSWORD;
     private javax.swing.JButton SEND;
-    private javax.swing.JRadioButton SHOW;
+    private javax.swing.JButton SEND1;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 
     private void metodoEntrar() {
